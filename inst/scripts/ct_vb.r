b106161fddb2f7f8b9ca9be8e46a31f9cf6c5585
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2021-07-01 06:53:31 +0000

    fix typo

--- inst/scripts/ct_vb.r
+++ inst/scripts/ct_vb.r
@@ -7,6 +7,6 @@ source(here::here("R", "ct_model.r"))
 suppressWarnings(dir.create(here::here("output")))
 
 for (target_gene in 1:2) {
-  fit <- fit_model("symptoms", target_gene, method = "meanfield")
+  fit <- fit_model("symptoms", target_gene, algorithm = "meanfield")
   saveRDS(fit, here::here("output", paste0("ct_", model, target_gene, "_vb.rds")))
 }
