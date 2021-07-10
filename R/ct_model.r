85765646820b57ef7d75f28a729c93a2c2b16e03
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2021-07-10 09:50:00 +0000

    update model

--- R/ct_model.r
+++ R/ct_model.r
@@ -10,15 +10,23 @@ models <-
                      family = student(),
                      sigma ~ (1 | age_group)),
        symptoms = bf(log10vl ~
-                       variant_alt_name + sex + s(age) + days_since_onset:variant_alt_name + dose_vaccine_variant,
+		       sex + age_group + dose_vaccine_variant + (days_since_onset):(sex + age_group + dose_vaccine_variant),
                      family = student(),
-                     sigma ~ (1 | age_group)))
+                     sigma ~ (1 | age_group)),
+       symptoms_smooth = bf(log10vl ~
+		       s(age) + sex + dose_vaccine_variant + days_since_onset:(sex + age_group + dose_vaccine_variant),
+                     family = student(),
+                     sigma ~ s(age)),
+       symptoms_random = bf(log10vl ~
+                        sex + days_since_onset + (1 + days_since_onset | age_group) + (1 + days_since_onset | dose_vaccine_variant) + sex:days_since_onset))
 
 variants <- readRDS(here::here("data", "variants_vl.rds")) %>%
   filter(is.na(product_display_type) | product_display_type != "MD",
          sex != "Unknown") %>%
   rename(Rt = median) %>%
-  mutate(dose_vaccine =
+  mutate(variant_alt_name = recode_factor(variant_alt_name,
+					  Wildtype = "_Wildtype"),
+	 dose_vaccine =
            if_else(dose == "dose0", "dose0",
                    paste(dose, product_display_type, sep = "_")),
          dose_vaccine = factor(dose_vaccine),
@@ -27,13 +35,18 @@ variants <- readRDS(here::here("data", "variants_vl.rds")) %>%
   filter(days_since_onset >= 0 & days_since_onset <= 28, dose != "dose2",
          !(variant_alt_name == "Wildtype" & dose != "dose0")) %>%
   mutate(dose_vaccine_variant =
-           if_else(dose_vaccine == "dose0", "_none",
-                   paste(dose_vaccine, variant_alt_name, sep = "_")))
+                   paste(dose_vaccine, variant_alt_name, sep = "_"))
 
-fit_model <- function(model, target_gene, ...) {
+fit_model <- function(model, target_gene, fit_to = "vl", sample = 1, ...) {
+  if (sample == 1) {
+	  data <- variants
+  } else {
+	  data <- sample_frac(variants, sample)
+  }
   fit <- brm(models[[model]],
-             data = variants %>%
-               rename(log10vl = !!quo_name(paste0("p2ch", target_gene, "cvl"))),
+             data = data %>%
+               rename(log10vl =
+		        !!quo_name(paste0("p2ch", target_gene, "c", fit_to))),
              chains = 2,
              cores = 2,
              iter = 2000,
