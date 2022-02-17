f86d406b02c047055918481e8ef3680460bc055e
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2022-02-17 14:23:47 +0000

    draft report

---
+++ R/utils.r
@@ -0,0 +1,20 @@
+estimate_means <- function(x, by) {
+
+  x[["var"]] <- do.call(interaction, lapply(by, function(y) x[[y]]))
+
+  means_fit <- mgcv::bam(ct ~ 0 + var, data = filter(x, !is.na(var)))
+  ## get CI
+  means <- coef(means_fit)
+  V <- vcov(means_fit, unconditional = TRUE)
+  se <- sqrt(diag(V))
+
+  tb <- tibble(
+    var = names(means),
+    estimate = means,
+    lower = means - 2 * se,
+    upper = means + 2 * se
+  )
+
+  tb %>%
+    separate(var, by, sep = "\\.")
+}
